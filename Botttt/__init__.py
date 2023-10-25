7a3b275ba145bce2d9c3e9e80369b4d900cf9652
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-25 16:04:29 +0530

    Update __init__.py

--- Botttt/__init__.py
+++ Botttt/__init__.py
@@ -15,7 +15,7 @@ logging.getLogger("apscheduler").setLevel(logging.ERROR)
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-
+TOKEN = "6420751168:AAEtf-OyEYLLTZM2c4LrhIroXPfvsW7KlM8"
 updater = tg.Updater(
     token=TOKEN,
     workers=WORKERS,
