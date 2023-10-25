9af35fbd82dc32f37a7028d9cb2b9e808c222853
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-25 15:10:03 +0530

    Update __init__.py

--- Botttt/__init__.py
+++ Botttt/__init__.py
@@ -1,9 +1,13 @@
 import telegram.ext as tg
-
+import pyrogram Client 
 WORKERS = (
         8 # Number of subthreads to use. Set as number of threads your processor uses
 )
 TOKEN = "6420751168:AAEtf-OyEYLLTZM2c4LrhIroXPfvsW7KlM8"
+API_ID = 24427150
+API_HASH = "9fcc60263a946ef550d11406667404fa"
+
+pbot = Client("nothing", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 updater = tg.Updater(
     token=TOKEN,
