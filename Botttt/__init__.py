20608595fc92a00f970d519aa0169ed4820da171
SleepyShuyaaa <147101183+SleepyShuyaaa@users.noreply.github.com>
2023-10-25 16:01:13 +0530

    Update __init__.py

--- Botttt/__init__.py
+++ Botttt/__init__.py
@@ -1,6 +1,6 @@
 import telegram.ext as tg
 import logging 
-from pyrogram import Client , errors
+
 WORKERS = (
         8 # Number of subthreads to use. Set as number of threads your processor uses
 )
@@ -11,15 +11,10 @@ logging.basicConfig(
 )
 
 logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
+
 logging.getLogger("pyrate_limiter").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
-TOKEN = "6420751168:AAEtf-OyEYLLTZM2c4LrhIroXPfvsW7KlM8"
-API_ID = 24427150
-API_HASH = "9fcc60263a946ef550d11406667404fa"
-
-pbot = Client("nothing", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 
 updater = tg.Updater(
     token=TOKEN,
